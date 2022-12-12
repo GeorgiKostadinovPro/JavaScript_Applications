4135585a569df967f706765750c00fd5eedde87f
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-12-12 19:19:36 +0200

    needs to refactor - 100/100

---
+++ 07_Routing/Exercises/01.Furniture/src/api/users.js
@@ -0,0 +1,23 @@
+import { post, get } from './api.js';
+import { setUserData } from '../utils.js';
+
+const endpoints = {
+    login: '/users/login',
+    register: '/users/register',
+    logout: '/users/logout',
+};
+
+export async function login(email, password) {
+    const res = await post(endpoints.login, { email, password });
+    setUserData({ email: res.email, id: res._id, token: res.accessToken });
+}
+
+export async function register(email, password) {
+    const res = await post(endpoints.register, { email, password });
+    setUserData({ email: res.email, id: res._id, token: res.accessToken });
+}
+
+export async function logout() {
+    await get(endpoints.logout);
+    sessionStorage.clear();
+}
