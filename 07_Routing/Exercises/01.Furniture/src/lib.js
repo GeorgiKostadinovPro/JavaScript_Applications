29538b3b2d917f22903beb08f693062d7bfd5184
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-12-12 23:29:09 +0200

    moifying

--- 07_Routing/Exercises/01.Furniture/src/lib.js
+++
@@ -1,5 +0,0 @@
-import { render, html, nothing} from './node_modules/lit-html/lit-html.js';
-import { until } from './node_modules/lit-html/directives/until.js';
-import page from './node_modules/page/page.mjs';
-
-export { render, html, nothing, page, until };
--- 07_Routing/Exercises/01.Furniture/src/utils.js
+++
@@ -1,8 +0,0 @@
-export function setUserData(data) {
-  sessionStorage.setItem('userdata', JSON.stringify(data));
-}
-
-export function getUserData() {
-  return JSON.parse(sessionStorage.getItem('userdata'));
-}
-
